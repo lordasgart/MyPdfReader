585579595d474c0e81dd2392a4ab8e25a5e4c21c
Markus Malz <markus.malz.ext@siemens.com>
2021-10-12 11:06:38 +0200

    Testing the test

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+// "C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe" --new-window --app="C:\Users\z004c1aw\Downloads\HWEGuidanceAndGovernanceForTestAllignment.pdf"
+//"C:\Program Files\Google\Chrome\Application\chrome.exe" --new-window --app="C:\Users\z004c1aw\Downloads\HWEGuidanceAndGovernanceForTestAllignment.pdf"
+
+using System.Diagnostics;
+
+Process.Start(@"C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe", @$"--new-window --app=""{args[0]}""");
